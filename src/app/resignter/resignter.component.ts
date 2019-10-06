56368b21fe0f1c143348fde9310019572a5f12c0
Đinh Văn Diện <learAngular@github.com>
2019-10-06 14:52:26 +0700

    first commit

---
+++ src/app/resignter/resignter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-resignter',
+  templateUrl: './resignter.component.html',
+  styleUrls: ['./resignter.component.css']
+})
+export class ResignterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/updateinfor/updateinfor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-updateinfor',
+  templateUrl: './updateinfor.component.html',
+  styleUrls: ['./updateinfor.component.css']
+})
+export class UpdateinforComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
